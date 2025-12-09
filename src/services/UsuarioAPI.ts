8b582bd3f6c6faeac159ffd676db47cfeff40dda
Pedro Avelar <90482434+Avelaar@users.noreply.github.com>
2025-12-09 17:39:56 -0300

    Update UsuarioAPI.ts

--- src/services/UsuarioAPI.ts
+++ src/services/UsuarioAPI.ts
@@ -2,5 +2,7 @@ import { apiClient } from "./api";
 
 export const UsuarioAPI = {
   criarConta: (dados: any) => apiClient.post("/usuarios/criar", dados),
+
+  login: (email: string, senha: string) =>
+    apiClient.post("/usuarios/login", { email, senha }),
 };
-    
